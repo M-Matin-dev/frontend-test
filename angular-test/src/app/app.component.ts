8a4242119c4a482f65d36805fd977f0f278b77a5
Mohammad Matinaghdam <m.matin.agh@gmail.com>
2020-09-12 23:44:18 +0430

    add app footer

--- angular-test/src/app/app.component.ts
+++ angular-test/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'talos-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-test';
-}
+export class AppComponent {}
